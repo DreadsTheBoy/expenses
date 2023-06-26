8b41e5dcf449c4d36ab3b4e65b13961b89fa58df
DreadsTheBoy <snake1allan@gmail.com>
2023-06-26 14:52:32 -0300

    Separation into widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,10 @@
 import 'package:flutter/material.dart';
 
-//Models
-import '../models/transaction.dart';
+//Components
+import 'package:expenses/components/transaction_user.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
-
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis de Corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,66 +12,16 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: const Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          const SizedBox(
+          SizedBox(
             child: Card(
               elevation: 1,
               child: Text('Gráfico'),
             ),
           ),
-          Column(
-            children: _transactions.map(
-              (tr) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            tr.date.toString(),
-                            style: const TextStyle(
-                              color: Colors.black54,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              },
-            ).toList(),
-          ),
+          TransactionUser()
         ],
       ),
     );
